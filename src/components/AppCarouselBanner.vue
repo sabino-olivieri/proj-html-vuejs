d310359cc6c36ee4de2f156ee85829e744ef42a1
AleDiBernardo <alessandrodibernardo47@gmail.com>
2024-05-14 11:32:52 +0200

    feat: AppCarouselBanner added

---
+++ src/components/AppCarouselBanner.vue
@@ -0,0 +1,78 @@
+<script>
+export default {
+  data() {
+    return {
+      activeIndex: 0,
+    };
+  },
+  props: {
+    array: Array,
+  },
+  created(){
+    this.startInt()
+  },
+  methods: {
+    getImagePath(Image) {
+      return new URL(`../assets/img/slider/${Image}`, import.meta.url).href;
+    },
+    showNext(){
+        this.activeIndex === this.array.length - 1
+        ? (this.activeIndex = 0)
+        : this.activeIndex++;
+    },
+    showPrev(){
+        this.activeIndex > 0
+        ? this.activeIndex--
+        : (this.activeIndex = this.array.length - 1);
+    },
+    startInt() {
+      this.interval = setInterval(() => {
+
+        this.showNext()
+      }, 5000);
+    },
+    
+  },
+};
+</script>
+
+<template>
+  <div id="carouselExample" class="carousel slide">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img :src="getImagePath(array[this.activeIndex])" class="d-block w-100" alt="..." />
+      </div>
+      <!-- <div class="carousel-item">
+        <img src="..." class="d-block w-100" alt="..." />
+      </div>
+      <div class="carousel-item">
+        <img src="..." class="d-block w-100" alt="..." />
+      </div> -->
+    </div>
+    <button
+      class="carousel-control-prev"
+      type="button"
+      data-bs-target="#carouselExample"
+      data-bs-slide="prev"
+      @click="showPrev()"
+
+    >
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Previous</span>
+    </button>
+    <button
+      class="carousel-control-next"
+      type="button"
+      data-bs-target="#carouselExample"
+      data-bs-slide="next"
+      @click="showNext()"
+    >
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+
+</style>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,12 +1,27 @@
 <script>
+import AppCarouselBanner from '../components/AppCarouselBanner.vue';
 export default {
+    components:{
+        AppCarouselBanner
+    },
+    data(){
+        return{
+            sliderImages: [
+                "image-1.jpg",
+                "image-2.png",
+                "image-3.jpg"
 
+            ]
+        }
+    }
 }
 </script>
 
 <template>
     <div>
-        <h1>Home Page</h1>
+        <!-- <h1>Home Page</h1> -->
+
+        <AppCarouselBanner :array="sliderImages"/>
     </div>
 </template>
 
