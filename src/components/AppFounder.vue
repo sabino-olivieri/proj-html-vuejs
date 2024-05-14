e465e40318ccdd6b1b297835881eafa430bc54f9
sabino-olivieri <sabino.olivieri.28@gmail.com>
2024-05-14 18:45:31 +0200

    feat: create AppFounder

---
+++ src/components/AppFounder.vue
@@ -0,0 +1,74 @@
+<script>
+export default {
+    data() {
+        return {
+            carouselImage: ["h1-img-01", "h1-img-02", "h1-img-03"],
+            imageActive: 0,
+        }
+    },
+    methods: {
+        getImagePath(Image) {
+            return new URL(`../assets/img/carousel-founder/${Image}.jpg`, import.meta.url).href;
+        }
+
+    },
+
+    created() {
+        setInterval(() => {
+            if(this.imageActive < this.carouselImage.length - 1) {
+                this.imageActive++;
+            } else {
+                this.imageActive = 0;
+            }
+        }, 5000)
+    }
+
+}
+</script>
+
+<template>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="ms_carousel">
+                    <div class="ms_carousel-item" :class="index === imageActive ? 'ms_active' : ''" v-for="(imageName, index) in carouselImage">
+                        <img :src="getImagePath(imageName)" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<style lang="scss" scoped>
+.container {
+    padding: 100px 0;
+
+
+    .ms_carousel {
+        width: 50%;
+        aspect-ratio: 4/3;
+        display: flex;
+
+        .ms_carousel-item {
+            width: 0;
+            
+            overflow: hidden;
+            transition: all 1s;
+
+            img {
+                object-fit: cover;
+                height: 100%;
+                width: 100%;
+            }
+        }
+
+        .ms_active {
+            width: 100%;
+        }
+        
+    }
+
+}
+</style>
--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,10 +1,36 @@
 <script>
+import AppFounder from '../components/AppFounder.vue';
+import AppOurSpecialties from '../components/AppOurSpecialties.vue';
 import { store } from '../store';
 
 export default {
-    data () {
+    components: { AppOurSpecialties, AppFounder },
+    data() {
         return {
             store,
+            ourSpecialtiesItem: [
+                {
+                    image: "svg-1.svg",
+                    subtitle: "knowing",
+                    text: "Lorem ipsum dolor sit amet, consectetur adipisicing"
+                },
+                {
+                    image: "svg-2.svg",
+                    subtitle: "Selling",
+                    text: "Lorem ipsum dolor sit amet, consectetur adipisicing"
+                },
+                {
+                    image: "svg-3.svg",
+                    subtitle: "Learning",
+                    text: "Lorem ipsum dolor sit amet, consectetur adipisicing"
+                },
+                {
+                    image: "svg-4.svg",
+                    subtitle: "Contact",
+                    text: "Lorem ipsum dolor sit amet, consectetur adipisicing"
+                },
+
+            ]
         }
     },
     created() {
@@ -18,20 +44,26 @@ export default {
     <section class="hero d-flex justify-content-center align-items-center w-100">
         <h1>About us</h1>
     </section>
+
+    <AppOurSpecialties :itemArray="ourSpecialtiesItem" />
+
+    <AppFounder />
 </template>
 
 <style lang="scss" scoped>
-    .hero {
-        background-image: url("../assets/img/about-us-title.jpg");
-        background-position: center;
-        background-size: cover;
-        height: 500px;
+.hero {
+    background-image: url("../assets/img/about-us-title.jpg");
+    background-position: center;
+    background-size: cover;
+    height: 500px;
 
-        h1 {
-            font-family: "Libre Baskerville", serif;
-            font-size: 4rem;
-            color: white;
-            font-weight: 700;
-        }
+    h1 {
+        font-family: "Libre Baskerville", serif;
+        font-size: 4rem;
+        color: white;
+        font-weight: 700;
     }
+}
+
+
 </style>
