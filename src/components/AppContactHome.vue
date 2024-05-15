3c3ffca0316ba1c1c81f3ed5325834187350d18f
sabino-olivieri <sabino.olivieri.28@gmail.com>
2024-05-15 19:35:05 +0200

    feat: form completed on AppContactHome

--- src/components/AppContactHome.vue
+++ src/components/AppContactHome.vue
@@ -1,14 +1,59 @@
 <script>
-
+import AppButton from './AppButton.vue'
+import AppTitle from './AppTitle.vue'
 export default {
+    components: { AppTitle, AppButton },
 
 }
 </script>
 
 <template>
-  <div>
+    <div class="container my-5 py-5">
+        <div class="row">
+            <div class="col">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi aut voluptate voluptatibus sapiente
+                corporis beatae doloremque alias eius explicabo enim id mollitia odio quisquam provident, vel facilis
+                aliquam, a accusantium.
+            </div>
 
-  </div>
+            <div class="col">
+                <AppTitle :justify="'justify-content-start'" :title="'Contact us'"
+                    :subTitle="'Lorem ipsum dolor sit amet, consectetur adipiscing elit'"
+                    :align="'align-items-start'" />
+
+                <form action="">
+                    <div class="col-12 d-flex justify-content-between flex-wrap gap-5">
+                        <div class="col-5">
+                            <input type="text" placeholder="Your Name" aria-label="Insert your name">
+                        </div>
+                        <div class="col-5">
+                            <input type="text" placeholder="Your Email" aria-label="Insert your email">
+                        </div>
+                        <div class="col-12">
+                            <textarea name="" id="" rows="5" placeholder="Your Message" aria-label="Insert your message"></textarea>
+                            
+                        </div>
+                    </div>
+                    <div class="col-12 mt-3">
+                        <AppButton :bgColor="'orange'" :innerText="'SEND'" :typeBtn="'submit'" />
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+    input, textarea {
+        outline: 0;
+        border: 0;
+        width: 100%;
+        background-color: transparent;
+        padding: 5px 20px;
+        border-bottom: 1px solid lightgray;
+        font-family: "Source Sans 3", sans-serif;
+        color: #606060;
+        font-weight: 300;
+    }
+
+</style>
--- src/components/AppTitle.vue
+++ src/components/AppTitle.vue
@@ -3,6 +3,14 @@ export default {
     props: {
         title: String,
         subTitle: String,
+        justify: {
+            type: String,
+            default: 'justify-content-center'
+        },
+        align: {
+            type: String,
+            default: 'align-items-center'
+        }
     }
 }
 </script>
@@ -10,11 +18,11 @@ export default {
 <template>
     <div class="container">
         <div class="row mt-5">
-            <div class="col-12 d-flex justify-content-center">
+            <div class="col-12 d-flex" :class="justify">
                 <h2>{{title}}</h2>
                 <span></span>
             </div>
-            <div class="col d-flex flex-column align-items-center">
+            <div class="col d-flex flex-column" :class="align">
                 <p>
                     {{ subTitle }}    
                 </p>
@@ -35,7 +43,7 @@ export default {
             font-family: "Libre Baskerville", serif;
             color: var(--ms-bold-color);
             font-weight: 700;
-            font-size: 3rem;
+            font-size: 2.8rem;
 
         }
 
@@ -53,7 +61,7 @@ export default {
         font-family: "Source Sans 3", sans-serif;
         color: #606060;
         font-weight: 300;
-        font-size: 1.5rem;
+        font-size: 1.3rem;
         margin: 0;
     }
 
